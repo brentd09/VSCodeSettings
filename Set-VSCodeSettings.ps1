f660da1fe33e47f845207e0358cef746bb32aa38
Brent Denny <brent.denny@ddls.com.au>
2018-05-29 12:08:38 +1000

    made changes to the vscode settings, snippets

--- Set-VSCodeSettings.ps1
+++ Set-VSCodeSettings.ps1
@@ -5,5 +5,9 @@ if (-not (Test-Path "$env:APPDATA\Code\User\")) {
   Start-Sleep 1
   Get-Process | Where-Object path -match 'code.exe$' | Stop-Process 
 }
-try {Copy-Item ./settings.json $env:APPDATA\Code\User\}
+# attempting to copy my UserSettings and Snippets into VSCode
+try {
+  Copy-Item .\settings.json $env:APPDATA\Code\User\ -ErrorAction stop
+  Copy-Item .\powershell.json $env:APPDATA\Code\User\snippets\ -ErrorAction Stop
+}
 catch {Write-Warning "File copy did not work"}
