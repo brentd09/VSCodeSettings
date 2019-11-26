2f519f6fa93589a3106cafc279d7af4a23b0a08b
Brent Denny <brent.denny@ddls.com.au>
2019-11-26 14:19:40 +1000

    fixed vscodesettings for user install

--- Set-VSCodeSettings.ps1
+++ Set-VSCodeSettings.ps1
@@ -1,31 +1,29 @@
 [CmdletBinding()]
 Param()
+$VSCODEInstalled = $false 
 if (Test-Path -Path "C:\Program files\Microsoft VS Code\Code.exe") {
+  $VSCodeExePath = "C:\Program files\Microsoft VS Code\Code.exe"
+  $VSCODEInstalled = $true
+}
+elseif (Test-Path -Path "$env:LOCALAPPDATA\Programs\Microsoft VS Code\Code.exe") {
+  $VSCodeExePath = "$env:LOCALAPPDATA\Programs\Microsoft VS Code\Code.exe"
+  $VSCODEInstalled = $true
+}
+if ($VSCODEInstalled -eq $true) {
   if (-not (Test-Path "$env:APPDATA\Code\User\")) {
-    & "C:\Program files\Microsoft VS Code\Code.exe"
+    & $VSCodeExePath
     Start-Sleep 5
     Get-Process | Where-Object path -match 'code.exe$' | Stop-Process 
   }
   # attempting to copy my UserSettings and Snippets into VSCode
-  if (Test-Path .\settings.json) {
-    try {
-      Copy-Item .\settings.json $env:APPDATA\Code\User\ -ErrorAction stop
-      Copy-Item .\keybindings.json $env:APPDATA\Code\User\ -ErrorAction stop
-      Copy-Item .\powershell.json $env:APPDATA\Code\User\snippets\ -ErrorAction Stop
-    }
-    catch {Write-Warning "File copy did not work"}
+  $ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+  try {
+    New-Item -Path $env:APPDATA\Code\User -Name snippets -ItemType Directory -Force -ErrorAction stop *> $null
+    Copy-Item $ScriptPath\settings.json $env:APPDATA\Code\User\ -Force -ErrorAction stop
+    Copy-Item $ScriptPath\keybindings.json $env:APPDATA\Code\User\ -Force -ErrorAction stop
+    Copy-Item $ScriptPath\powershell.json $env:APPDATA\Code\User\snippets\ -Recurse -Force -ErrorAction Stop
   }
-  else {
-    try {
-      $drives = (Get-PSDrive -PSProvider filesystem | Where-Object {$_.used -and -not $_.displayroot}).Root
-      $Location = Get-ChildItem -Path $drives -Filter set-vscodesettings.ps1 -Recurse -ErrorAction SilentlyContinue
-      $Directory = $Location.Directory.FullName
-      Copy-Item $Directory\settings.json $env:APPDATA\Code\User\ -ErrorAction stop
-      Copy-Item $Directory\keybindings.json $env:APPDATA\Code\User\ -ErrorAction stop
-      Copy-Item $Directory\powershell.json $env:APPDATA\Code\User\snippets\ -ErrorAction Stop
-    }
-    catch {Write-Warning "File copy did not work"}
-  }
+  catch {Write-Warning "File copy did not work"}
   $GitConfig = git config --list | Where-Object {$_ -match 'user\.name'}
   if ($GitConfig -notmatch 'user\.name\=Brent Denny') {
     try {
